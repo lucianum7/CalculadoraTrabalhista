f2940a3c5841382026b0f59b644d0b774255da65
Lucianum <lucianummagnum@gmail.com.unlinked202608131840>
2026-08-13 17:44:49 -0300

    Sync repository files

---
+++ src/calculadora_trabalhista/pjecalc_compat/__init__.py
@@ -0,0 +1,5 @@
+"""Comparação estruturada com resultados de conferência, sem alegar equivalência."""
+
+from .comparator import ComparisonRow, compare_results
+
+__all__ = ["ComparisonRow", "compare_results"]
